b464ee768ca31be4011d8558e2552b215c6c0102
root <ziv@anyvision.co>
2018-12-19 18:09:02 +0200

    Smarter entrypoint to include external-ip parameter

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # If command starts with an option, prepend with turnserver binary.
 if [ "${1:0:1}" == '-' ]; then
-  set -- turnserver "$@"
+  set -- turnserver --external-ip="${EXTERNAL_IP:-$(hostname -i)}" "$@"
 fi
 
 exec $(eval "echo $@")
