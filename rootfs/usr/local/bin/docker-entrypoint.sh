3761608c179e04d237d255512d6cf974d222e0bd
digger18 <digger18@users.noreply.github.com>
2018-12-26 14:47:48 +0200

    Update docker-entrypoint.sh

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -eu
 
-IP="${EXTERNAL_IP:-$(ip route get "$(dig ${WEBRTC_HOST:-webrtc.tls.ai} +short)" | awk '{print $NF;exit}')"
+DESTINATION_IP="$(dig ${WEBRTC_HOST:-webrtc.tls.ai} +short)"
+ROUTE_IP="$(ip route get ${DESTINATION_IP} | awk '{print $NF;exit}')"
+IP="${EXTERNAL_IP:-$ROUTE_IP}"
 
 # If command starts with an option, prepend with turnserver binary.
 if [ "${1:0:1}" == '-' ]; then
