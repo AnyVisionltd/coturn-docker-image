2655d20dc62b1ae34cb47502745d838c4199f89e
digger18 <digger18@users.noreply.github.com>
2018-12-26 13:50:25 +0200

    Update docker-entrypoint.sh

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+IP="${EXTERNAL_IP:-$(ip route get 1 | awk '{print $NF;exit}')}"
+
 # If command starts with an option, prepend with turnserver binary.
 if [ "${1:0:1}" == '-' ]; then
-  set -- turnserver --listening-ip="${EXTERNAL_IP:-$(ip route get 1 | awk '{print $NF;exit}')}" --external-ip="${EXTERNAL_IP:-$(ip route get 1 | awk '{print $NF;exit}')}" "$@"
+  set -- turnserver --listening-ip=$IP --relay-ip=$IP --external-ip=$IP "$@"
 fi
 
 exec $(eval "echo $@")
