c62c1a8a25021501286e9a12d3c3169e55645b88
root <ziv@anyvision.co>
2018-12-30 20:22:07 +0200

    Updated Docker and entrypoints.

--- rootfs/usr/local/bin/detect-external-ip.sh
+++ rootfs/usr/local/bin/detect-external-ip.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ -z "$REAL_EXTERNAL_IP" ]; then
-  export REAL_EXTERNAL_IP="$(curl -4 https://icanhazip.com 2>/dev/null)"
-fi
-
-exec echo "$REAL_EXTERNAL_IP"
+exec echo "$(curl -4 https://icanhazip.com 2>/dev/null)"
--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-set -eu
 
-DESTINATION_IP="$(dig ${WEBRTC_HOST:-webrtc.tls.ai} +short)"
-ROUTE_IP="$(ip route get ${DESTINATION_IP} | awk '{print $NF;exit}')"
-IP="${EXTERNAL_IP:-$ROUTE_IP}"
-
-# If command starts with an option, prepend with turnserver binary.
-if [ "${1:0:1}" == '-' ]; then
-  set -- turnserver --listening-ip=$IP --relay-ip=$IP --external-ip=$IP "$@"
+if [ -z "${EXTERNAL_IP}" ]; then
+  if [ "${DETECT_PUBLIC_IP}" = true ]; then
+    EXTERNAL_IP="$(detect-external-ip.sh)"
+  else
+    DESTINATION_IP="$(dig ${WEBRTC_HOST:-webrtc.tls.ai} +short)"
+    EXTERNAL_IP="$(ip route get ${DESTINATION_IP} | awk '{print $NF;exit}')"
+  fi
 fi
 
-exec $(eval "echo $@")
+exec turnserver --external-ip ${EXTERNAL_IP} ${COTURN_CMD}
