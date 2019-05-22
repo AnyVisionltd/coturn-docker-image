ba16ab07cc9b48c4090da81256415283e37dbb8d
hagay <hagayd@anyvision.co>
2019-05-22 21:32:37 +0300

    new coturn source

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${EXTERNAL_IP}" ]; then
-  EXTERNAL_IP="$(ip route get $(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+') | grep -oP 'src \K\S+')"
+  EXTERNAL_IP=$(ip route get $(ip route show 0.0.0.0/0|grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")|awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')
 fi
 
 exec turnserver --external-ip ${EXTERNAL_IP} ${COTURN_CMD}
